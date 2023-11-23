78fa05b2f0284b4173b611c26d123f36803b0216
guizimo <17680262548@163.com>
2023-11-23 14:59:40 +0800

    feat:移除头部标题栏

--- electron/moudles/WinMain.ts
+++ electron/moudles/WinMain.ts
@@ -23,9 +23,9 @@ class WinMain {
     width: 1200,
     height: 800,
     show: false, // 是否在创建时显示, 默认值为 true
-    frame: true, // 是否有边框
+    frame: false, // 是否有边框
     center: true, // 是否在屏幕居中
-    hasShadow: false, // 窗口是否有阴影. 默认值为 true
+    hasShadow: true, // 窗口是否有阴影. 默认值为 true
     resizable: true, // 是否允许拉伸大小
     fullscreenable: true, // 是否允许全屏，为 false 则插件 screenfull 不起作用
     autoHideMenuBar: true, // 自动隐藏菜单栏, 除非按了 Alt 键, 默认值为 false
